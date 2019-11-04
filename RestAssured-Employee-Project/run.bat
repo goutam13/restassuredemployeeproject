b5a873ff3a0649fb8b3edec931ae49828c332947
Goutam Karmakar <goutam.techno@gmail.com>
2019-11-04 22:46:11 +0530

    My First Commit

---
+++ RestAssured-Employee-Project/run.bat
@@ -0,0 +1,2 @@
+cd C:\Users\gkarmaka\eclipse-workspace\RestAssured-Employee-Project
+mvn clean install
---
+++ RestAssured-Employee-Project/src/test/java/com/employeeapi/testcase/TC002_Get_Single_Employee_Record.java
@@ -0,0 +1,90 @@
+package com.employeeapi.testcase;
+
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+import com.employeeapi.base.TestBase;
+
+import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+public class TC002_Get_Single_Employee_Record extends TestBase{
+
+	RequestSpecification httpRequest;
+	Response response;
+
+		
+	@BeforeClass
+	void getEmployeeData() throws InterruptedException
+	{
+		logger.info("*********Started TC002_Get_Single_Employee_Record **********");
+		
+		RestAssured.baseURI = "http://dummy.restapiexample.com/api/v1";
+		httpRequest = RestAssured.given();
+		
+		response = httpRequest.request(Method.GET, "/employee/"+empID);
+		
+		Thread.sleep(10000);
+	}
+	
+	@Test
+	void checkResposeBody()
+	{
+		String responseBody = response.getBody().asString();
+		Assert.assertEquals(responseBody.contains(empID), true);
+	}
+		
+	@Test
+	void checkStatusCode()
+	{
+		int statusCode = response.getStatusCode(); // Gettng status code
+		Assert.assertEquals(statusCode, 200);
+	}
+	
+	@Test
+	void checkResponseTime()
+	{
+		long responseTime = response.getTime(); // Getting status Line
+		Assert.assertTrue(responseTime<6000);
+		
+	}
+		
+	@Test
+	void checkstatusLine()
+	{
+		String statusLine = response.getStatusLine(); // Gettng status Line
+		Assert.assertEquals(statusLine, "HTTP/1.1 200 OK");
+		
+	}
+	
+	@Test
+	void checkContentType()
+	{
+		String contentType = response.header("Content-Type");
+		Assert.assertEquals(contentType, "text/html; charset=UTF-8");
+	}
+
+	@Test
+	void checkserverType()
+	{
+		String serverType = response.header("Server");
+		Assert.assertEquals(serverType, "nginx/1.16.0");
+	}
+
+	@Test
+	void checkContentLenght()
+	{
+		String contentLength = response.header("Content-Length");
+		Assert.assertTrue(Integer.parseInt(contentLength)<1500);
+	}
+	
+	@AfterClass
+	void tearDown()
+	{
+		logger.info("*********  Finished TC002_Get_Single_Employee_Record **********");
+	}
+	
+}
